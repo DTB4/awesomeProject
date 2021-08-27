61f6436adb0ff5c596d7acfdb472b044b94be84a
barbarian <darkerbarbarian@gmail.com>
2021-08-27 22:21:41 +0300

    remove viper make my own .env parser added my own logger as dependency from gitHub

--- cmd/main.go
+++ cmd/main.go
@@ -5,22 +5,27 @@ import (
 	"awesomeProject/handlers"
 	"awesomeProject/repository"
 	"awesomeProject/services"
-	"github.com/spf13/viper"
+	"fmt"
+	"github.com/DTB4/logger/v2"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	if err := initConfig(); err != nil {
+	err := getConfig()
+	if err != nil {
 		log.Fatalf("Fail to initialise configs: %s", err.Error())
 	}
+	myLogger := logger.NewLogger(os.Getenv("LOGS_DIRECTORY_PATH"))
 
 	db := dbconstructor.NewDB()
 	userRepository := repository.NewUserRepository(db)
 	orderRepository := repository.NewOrderRepository(db)
 	userService := services.NewUserService(orderRepository, userRepository)
 	tokenService := services.NewTokenService()
-	profileHandler := handlers.NewProfileHandler(userService, tokenService)
+	profileHandler := handlers.NewProfileHandler(userService, tokenService, myLogger)
 
 	http.HandleFunc("/getall", profileHandler.GetAll)
 	http.HandleFunc("/registration", profileHandler.CreateNewUser)
@@ -28,12 +33,27 @@ func main() {
 	http.HandleFunc("/editprofile", profileHandler.TokenCheck(profileHandler.EditUserProfile))
 	http.HandleFunc("/login", profileHandler.Login)
 
-	log.Fatal(http.ListenAndServe(viper.GetString("HTTP_PORT"), nil))
+	log.Fatal(http.ListenAndServe(os.Getenv("HTTP_PORT"), nil))
 }
 
-func initConfig() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("config")
-	return viper.ReadInConfig()
+func getConfig() error {
+	b, err := os.ReadFile("configs/config.env")
+	if err != nil {
+		return err
+	}
+	s := string(b)
+	lines := strings.Split(s, "\n")
+	for i := range lines {
+		if lines[i] == "" {
+			break
+		}
+		params := strings.Split(lines[i], "=")
+		err = os.Setenv(params[0], params[1])
+		if err != nil {
+			fmt.Println("failed to set env parameter")
+		}
+		//fmt.Println(params[1])
+	}
 
+	return nil
 }
